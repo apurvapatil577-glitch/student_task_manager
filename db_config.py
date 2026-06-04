74b6d500c53a61c909380c0aaebf8a42e23aaf0b
Apurva Patil <apurvapatil577@gmail.com>
2026-06-04 09:52:47 +0530

    Connect TiDB database

--- db_config.py
+++ db_config.py
@@ -1,14 +1,16 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='root@1234',
-            database='student_task_manager'
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         return connection
     except mysql.connector.Error as err:
         print("Database Error:", err)
-        return None 
+        return None
